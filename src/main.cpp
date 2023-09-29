9f69fb5809b78c7751106989a7429aad2f8009e0
nhaar <nhaar224131@gmail.com>
2023-09-29 09:47:36 -0300

    implement argv

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <sstream>
 #include <filesystem>
+#include <shlobj.h>
 
 namespace fs = std::filesystem;
 using namespace std;
@@ -349,7 +350,6 @@ class ProbabilityDistribution {
         // add +1 to include the maximum value as well, due to 0-indexsng
         // length is calculated from the range and bin size
         length = (max + 1 - min) / interval;
-        std::cout << "length " << length << std::endl;
 
         // actual distribution is just an array where each element of the array represents a "x" position
         // and the value is the number of time it appears in that "x" position
@@ -370,8 +370,6 @@ public:
             if (current > max) max = current;
             else if (current < min) min = current;
         }
-        std::cout << max << std::endl;
-        std::cout << min << std::endl;
         build_dist(values, size);
     }
 
@@ -565,20 +563,90 @@ public:
     }
 };
 
-int main () {
-    // seed program
-    std::srand(time(0));
-
-    // demo
-
-    RecordingReader reader(".\\recordings");
-    Times times = reader.get_best();    
-    Ruins simulator(times);
+int main (int arc, char *argv[]) {
+    // reading argv for the settings
+    PWSTR app_data;
+    HRESULT hr = SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, NULL, &app_data);
+    wstring_convert<codecvt_utf8_utf16<wchar_t>> converter;
+    string dir = converter.to_bytes(app_data) + "\\UNDERTALE_linux_steamver\\recordings";
+    bool calculate_chance = false;
+    bool get_avg = false;
+    bool get_stdev = false;
+    int chance_min = -1;
+    int chance_max = -1;
+    bool use_best = false;
     int simulations = 1'000'000;
+
+    /*
+    command line arguments
+    -d "..." -> set directory
+    -c -> sets calculate chance to true
+    -s -> set number of simulations
+    -a -> calculate average
+    -e -> calculate error (stdev)
+    -n ... -> set min time in frames
+    -x ... -> set max time in frames
+    -b -> set use_best to true
+    */
+
+    int cur_arg = 1;
+    while (cur_arg < arc) {
+        switch (argv[cur_arg][1]) {
+            case 'd':
+                cur_arg++;
+                dir = argv[cur_arg];
+                break;
+            case 'c':
+                calculate_chance = true;
+                break;
+            case 's':
+                cur_arg++;
+                simulations = stoi(argv[cur_arg]);
+                break;
+            case 'a':
+                get_avg = true;
+                break;
+            case 'e':
+                get_stdev = true;
+                break;
+            case 'n':
+                cur_arg++;
+                chance_min = stoi(argv[cur_arg]);
+                break;
+            case 'x':
+                cur_arg++;
+                chance_max = stoi(argv[cur_arg]);
+                break;
+            case 'b':
+                use_best = true;
+                break;
+        }
+        cur_arg++;
+    }
+
+    // seed program
+    srand(time(0));
+
+    RecordingReader reader(dir);
+    Times times;
+    if (use_best) times = reader.get_best();
+    else times = reader.get_average();
+    Ruins simulator(times);
     ProbabilityDistribution dist = simulator.get_dist(simulations);
-    cout << dist.get_average() << endl;
-    cout << dist.get_stdev() << endl;
-    cout << dist.get_chance(5 * 60 * 30, 6 * 60 * 30);
+    if (calculate_chance) {
+        double chance;
+        if (chance_min == -1 && chance_max == -1) chance = 1;
+        else if (chance_min == -1) chance = dist.get_chance_up_to(chance_max);
+        else if (chance_max == -1) chance = dist.get_chance_from(chance_min);
+        else chance = dist.get_chance(chance_min, chance_max);
+        cout << "Chance: " << chance << endl;;
+    }
+    if (get_avg) {
+        cout << "Average: " << dist.get_average() << endl;
+    }
+    if (get_stdev) {
+        cout << "Standard Deviation: " << dist.get_stdev() << endl;
+    }
 
     return 0;
 }
